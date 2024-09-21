5c2aa39ea282d5ed1a733d9a928ac6616a8ef505
christinadeng-hellofresh <christina.deng@hellofresh.ca>
2024-09-20 20:39:56 -0400

    feat: update homepage

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import ToolsPage from "./pages/Tools";
 
 const App = () => {
   return (
-    <Router basename="/react-app">
+    <Router basename="/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
